31e241b6c4d7157a4199b2474305f55fad10ca4d
rybanin <rybanin@protei.ru>
2019-09-06 18:06:54 +0300

    eeah, its working

--- modeling/lib/BRV.py
+++ modeling/lib/BRV.py
@@ -1,28 +1,19 @@
 import math
 import time
 
-def _random_base(a, m, y, c):
+def _random_base(m, y):
     return (1664525 * y + 1) % m
 
 
 def random(after_dot: int = 5, count: int = 10):
     main_list = []
-    nod_dict = {
-        5: {"c": 33343, "m": 2 ** 32},
-        4: {"c": 7789, "m": 2 ** 32},
-        3: {"c": 557, "m": 2 ** 32},
-        2: {"c": 11, "m": 2 ** 32},
-        1: {"c": 7, "m": 2 ** 32}
-    }
     y = time.time() % 1
-    #y = float('{:.1f}'.format(y))
     print(y)
-    c = nod_dict.get(after_dot).get("c")
-    m = nod_dict.get(after_dot).get("m")
+    m = 2 ** 32
     for i in range(count):
-        result = _random_base(3, m, y, c) % 1
+        result = _random_base(m, y) % 1
         y = result
-        result = float('{:.5f}'.format(result))
+        result = float('{:.{}f}'.format(result, after_dot))
         main_list.append(result)
     return main_list
 
